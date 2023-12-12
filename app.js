ad416c0c8e7b7cb1f642abda400ea2dd3148d7f8
bsenator <beyzasenaerkoc@gmail.com>
2023-12-12 16:52:06 +0300

    update 2.0

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-import express from "express"
+import express from "express";
 
-const app = express() //expressi çalıştıracağımız değişkeni oluşturdukl
-const port= 3000
+const app = express(); //expressi çalıştıracağımız değişkeni oluşturdukl
+const port= 3000;
+
+//ejs template engine
+app.set('view engine', 'ejs');
+
+//static files middleware (yapmak istedigim ek islemler icin mw)
+//publicin icindekiler static dedik yani bunu tanımladık
+app.use(express.static('public'));
 
 
-app.get("/", (req,res) =>{
-    res.send("project start");
+app.get('/', (req, res) =>{
+    res.render('index');
+});
+
+app.get('/about', (req, res) =>{
+    res.render('about');
 });
 
 
 //get metodu, ilgili yonlendirmeyi yapar
-// projemizin kok adresindeki istek gönderdiğimizde get istedği
-// yapması gereken seyi söylüyoruz
+// projemizin kok adresindeki istek gönderdiğimizde get isteği yapması gereken seyi söylüyoruz
 // ikinci parametre ise callback fonsiyonu yazarız
 
 //node js web uygulaması bir request response donsunden ibarettir.
