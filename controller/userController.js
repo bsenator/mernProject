448813c851618a8c73ad9c39147f49afab2d36da
bsenator <beyzasenaerkoc@gmail.com>
2023-12-18 16:01:35 +0300

    login process is succesfull

--- controller/userController.js
+++ controller/userController.js
@@ -1,56 +1,54 @@
 import User from "../models/userModel.js";
-
+import bcrypt from 'bcrypt';
 
 const createUser = async (req, res) => {
   try {
-    const user = await User.create(req.body); // burada bi sorun var yarın videodan izle bi veritabanına baglanıyor yazdırıyor ama webte kullanıcya gostermiyor
-
+    const user = await User.create(req.body);
+   
     res.status(201).json({
-      succeded: true,
-      photo,
+      succeeded: true,
+      user,
     });
   } catch (error) {
     res.status(500).json({
-      succeded: false,
+      succeeded: false,
       error,
     });
   }
 };
-
+ 
 const loginUser = async (req, res) => {
   try {
-    const {username, password} = req.body;
+    const { username, password } = req.body;
    
-    const user = User.findOnve({username})
+    const user =  await User.findOne({ username });
 
     let same = false;
 
-    if(user) {
-      same = await bycrpt.compare(password, user.password)
+    if (user) {
+      same = await bcrypt.compare(password, user.password);
     } else {
       return res.status(401).json({
-        succeded: false,
-        error: 'here is no such user',
+        succeeded: false,
+        error: 'No such user',
       });
     }
 
-    if(user) {
-      res.status(200).send("u are logged in") 
+    if (same) {
+      res.status(200).send("You are logged in");
     } else {
       res.status(401).json({
-        succeded: false,
-        error: 'here is no such user',
+        succeeded: false,
+        error: 'Password does not match',
       });
     }
 
   } catch (error) {
     res.status(500).json({
-      succeded: false,
-      error, 'password are not match'
+      succeeded: false,
+      error: 'Internal server error',
     });
   }
 };
 
-
-
-export {createUser, loginUser};
+export { createUser, loginUser };
